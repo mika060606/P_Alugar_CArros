608cdb2c57c988d62d34246ecf714c08f0db257d
mika060606 <MichaelFelipenvs@gmail.com>
2026-04-27 13:16:36 +0100

    nnn

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_folder='Imagens', static_url_path='/Imagens')
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI']="postgresql://aluguel_carros_user:Q4ud9pshTLuXzXIEsZtPjeFibpZ2QIam@dpg-d7nih4cvikkc73b9o8n0-a/aluguel_carros"
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://aluguel_carros_user:Q4ud9pshTLuXzXIEsZtPjeFibpZ2QIam@dpg-d7nih4cvikkc73b9o8n0-a.oregon-postgres.render.com/aluguel_carros"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
