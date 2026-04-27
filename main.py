f8fee5f089ac6da03b5dc2f27b30cd4845bd1f2b
mika060606 <MichaelFelipenvs@gmail.com>
2026-04-27 13:24:09 +0100

    erro_sintaxe

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ app = Flask(__name__, static_folder='Imagens', static_url_path='/Imagens')
 app.config['SQLALCHEMY_DATABASE_URI']="postgresql://aluguel_carros_user:Q4ud9pshTLuXzXIEsZtPjeFibpZ2QIam@dpg-d7nih4cvikkc73b9o8n0-a.oregon-postgres.render.com/aluguel_carros"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "connect_args": {"sslmode": "require"}
 }
+
+db = SQLAlchemy(app)
+
+
 
 with app.app_context():
     db.create_all()
