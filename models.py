fc09b5e2da61074e10a221ca58278ece81749635
mika060606 <MichaelFelipenvs@gmail.com>
2026-06-18 12:12:02 +0100

    _Sera

--- models.py
+++ models.py
@@ -7,12 +7,12 @@ from db import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(500), nullable=False)
-    sobrenome = db.Column(db.String(500), nullable=False)
-    email = db.Column(db.String(500), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    sobrenome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     numero_telefone = db.Column(db.String(15), nullable=False)
-    senha = db.Column(db.String(500), nullable=False)
-    cnf_senha = db.Column(db.String(500), nullable=False)
+    senha = db.Column(db.Text(500), nullable=False)
+    cnf_senha = db.Column(db.String(100), nullable=False)
     termos = db.Column(db.Boolean, nullable=False)
 
 
